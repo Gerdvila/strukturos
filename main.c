751117cd65fc5c870d31d83d7ec24ef91f0f804c
Gerdvila <119859311+Gerdvila@users.noreply.github.com>
2022-12-08 00:29:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -9,33 +9,38 @@ struct Plokstuma
 
 };
 
-int eiluciuKiekis(char *filename);
-void uzpildymas(char *filename,struct Plokstuma *masyvas,int eilutes);
-void staciakampiai(struct Plokstuma *masyvas, int eilutes);
+int EiluciuKiekis(char *filename);
+void Uzpildymas(char *filename,struct Plokstuma *masyvas,int eilutes);
+void staciakampiai(struct Plokstuma *masyvas,int eilutes, int *count);
+void Atlaisvinimas(struct Plokstuma *masyvas, int eilutes);
+//void spausdinimas(struct Plokstuma *masyvas, int eilutes, int *count);
 
 int main()
 {
     char filename[255];
     scanf("%s",filename);
     struct Plokstuma *masyvas;
-    int eilutes=eiluciuKiekis(filename);;
+    int eilutes=EiluciuKiekis(filename);;
     masyvas=malloc(eilutes*sizeof(struct Plokstuma));
+    int *count;
+    int *plot;
     if(masyvas!=NULL)
     {
-        uzpildymas(filename,masyvas,eilutes);
-        free(masyvas);
+        Uzpildymas(filename,masyvas,eilutes);
+        staciakampiai(masyvas,eilutes,count);
+        printf("%d",*count);
     }
     else
     {
         printf("Nepavyko alokuoti atminties");
     }
-
+    Atlaisvinimas(masyvas,eilutes);
    // int *count;
-    staciakampiai(masyvas,eilutes);
+    
 
     return 0;
 }
-int eiluciuKiekis(char *filename)
+int EiluciuKiekis(char *filename)
 {
     int ch, count = 0;
     FILE* fptr = fopen(filename, "r");
@@ -50,12 +55,11 @@ int eiluciuKiekis(char *filename)
         }
         while (ch != EOF);
     }
-
     fclose(fptr);
-    return count+1;
+    return count;
 }
 
-void uzpildymas(char *filename,struct Plokstuma *masyvas,int eilutes)
+void Uzpildymas(char *filename,struct Plokstuma *masyvas,int eilutes)
 {
     FILE *fp=fopen(filename,"r");
     if(fp!=NULL)
@@ -63,9 +67,7 @@ void uzpildymas(char *filename,struct Plokstuma *masyvas,int eilutes)
         for(int i=0; i<eilutes;i++)
         {
            fscanf(fp,"%d %d",&masyvas[i].x,&masyvas[i].y);
-
-           printf("%4d %4d\n",masyvas[i].x,masyvas[i].y);
-           
+    
         }
         
     }
@@ -77,15 +79,44 @@ void uzpildymas(char *filename,struct Plokstuma *masyvas,int eilutes)
     fclose(fp);
 }
 
-void staciakampiai(struct Plokstuma *masyvas, int eilutes)
+void staciakampiai(struct Plokstuma *masyvas,int eilutes, int *count)
 {
-printf("kitoje funkcijoje : \n");
+int kintamasis=0;
     for(int i=0;i<eilutes;i++)
     {
-       printf("%4d %4d\n",masyvas[i].x,masyvas[i].y);
+       for(int j=0+i;j<eilutes;j++) 
+       {
+            if(masyvas[i].x==masyvas[j].x && masyvas[i].y!=masyvas[j].y)
+            {
+                for(int k=0+j;k<eilutes;k++)
+                {
+                    for(int h=0+k;h<eilutes;h++)
+                    {
+                    if(masyvas[k].x==masyvas[h].x && masyvas[k].x!=masyvas[i].x)
+                        {
+                            if(masyvas[k].y==masyvas[i].y && masyvas[h].y==masyvas[j].y ||masyvas[h].y==masyvas[i].y && masyvas[k].y==masyvas[j].y )
+                            {
+                            
+                                kintamasis++;
+                                
+                            }
+                        }
+                    else continue;
+                    }
+                }
+            }
 
+        }        
     }
-            
+
+*count=kintamasis;
 }
 
-    
+void Atlaisvinimas(struct Plokstuma *masyvas, int eilutes)
+{
+    for(int i=0;i<eilutes;i++)
+    {
+            free(masyvas+i);
+            
+    }
+}
